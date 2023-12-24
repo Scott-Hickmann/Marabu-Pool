516b1f7fff94fbcd2ad3a17fa6dc8fecbf87aead
Scott Hickmann <scotthickmann21@gmail.com>
2023-12-23 18:08:41 -0800

    Marabu client update

--- client.js
+++ client.js
@@ -35,6 +35,10 @@ function updateChainTip(chainTipObject) {
   client.sendMessage({
     type: 'getmempool'
   })
+  // client.sendMessage({
+  //   type: 'object',
+  //   object: {"inputs":[{"outpoint":{"index":0,"txid":"84a2635e06fe5bd5f0df45237de6f16c967197da841057264cb96f1678eaab60"},"sig":"4b0f0cf684db8001dd01feb8303f3ef0d64ce7aa274b0d85089bcfe15d6139eb1282ba1a5d230a617458caa23726bd1ee667f68c497b89675fb286eacb4abe0e"}],"outputs":[{"pubkey":"4a78717bd8f31091c8dfe8c2b8c3614a0d6ac77ec27e560546cf50ccb3ebb6b1","value":50000000000000}],"type":"transaction"}
+  // })
   client.netSocket.on('error', e => console.log(e))
   client.on('message', (messageStr) => {
     const message = JSON.parse(messageStr)
